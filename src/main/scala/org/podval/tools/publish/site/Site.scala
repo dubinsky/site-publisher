ce464c00da1576861f5cf240dc0db2f1793d7693
Leonid Dubinsky <dub@podval.org>
2026-08-11 13:14:45 -0400

    - cleanup;

--- src/main/scala/org/podval/tools/publish/site/Site.scala
+++ src/main/scala/org/podval/tools/publish/site/Site.scala
@@ -267,8 +267,8 @@ object Site:
 
   @main def generate(): Unit = Site(SiteOptions(
     sourceDirectoryPath =
-//      "/home/dub/OpenTorah/chumashquestions.org/",
-      "/home/dub/OpenTorah/opentorah.org/docs",
+      "/home/dub/OpenTorah/chumashquestions.org/",
+//      "/home/dub/OpenTorah/opentorah.org/docs",
     //  "/home/dub/OpenTorah/alter-rebbe.org"
 //      "/home/dub/Podval/dub.podval.org",
     //  "/home/dub/Podval/www.podval.org"
