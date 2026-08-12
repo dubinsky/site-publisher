669c74e2d2a3c925aef9e7736859fee25e2671d5
Leonid Dubinsky <dub@podval.org>
2026-08-12 12:57:29 -0400

    - cleanup: TeiMarkup;

--- src/main/scala/org/podval/tools/publish/markup/TeiMarkup.scala
+++ src/main/scala/org/podval/tools/publish/markup/TeiMarkup.scala
@@ -4,8 +4,10 @@ import org.podval.tei.{EntityKind, TeiXmlDialect}
 import org.podval.tools.publish.page.{MarkupPage, PageSource}
 import org.podval.tools.publish.site.Site
 import org.podval.tools.publish.util.IdGenerator
-import org.podval.xml.{Html, Xml, Xml2Html}
+import org.podval.xml.{Html, Xml, Xml2Html, XmlUtil}
 import org.podval.xml.XmlUtil.*
+import zio.blocks.chunk.Chunk
+
 import java.io.File
 
 object TeiMarkup extends Markup(
@@ -37,31 +39,22 @@ object TeiMarkup extends Markup(
     source: PageSource,
     xml: Xml.Element
   ): (Xml.Element, Option[Xml.Element]) =
+    // TODO extract title
     val tei2Html: Xml2Html = Xml2Html("tei")
     val footnoteCorrelationIds: IdGenerator = IdGenerator("")
+
     val result: Xml.Element = xmlDialect.transform(xml, element =>
       var result: Xml.Element = element
 
       result = tei2Html.convert(result)
 
-      // TODO merge into a match on the name:
+      result = result.setChildren(XmlUtil.convertElements(result.getChildren, convertFootnote(_, footnoteCorrelationIds)))
+
       result = convertSpecial(result)
-      if EntityKind.values.exists(_.nameElement == result.getName) then
-        // TODO turn those into As *only* if 'ref' attribute is present!
-        result = renameElement("a", copyAttribute("ref", "href", element))
-
-      if element.getName == "pb" then
-        // TODO convert 'n' attribute?
-        result = renameElement("a", element.setText(facsimileSymbol))
-
-      if result.getName == "note" && result.get("place").contains("end") then
-        // TODO generate correlationId and replace footnote element with link stub *and* body stub with the same correlationId
-        ()
-
-      if result.getName == "div" then Section.mark(result)
 
       result
     )
+
     (result, None)
 
   private def convertSpecial(element: Xml.Element): Xml.Element = element.getName match
@@ -77,5 +70,28 @@ object TeiMarkup extends Markup(
     case "ref" | "ptr" =>
       renameElement("a", copyAttribute("target", "href", element))
 
+    case name if EntityKind.values.exists(_.nameElement == name) =>
+      // TODO turn those into As *only* if 'ref' attribute is present!
+      renameElement("a", copyAttribute("ref", "href", element))
+
+    case "pb" =>
+      // TODO convert 'n' attribute?
+      renameElement("a", element.setText(facsimileSymbol))
+
+    case "div" =>
+      Section.mark(element)
+
     case _ =>
       element
+
+  // Footnotes in TEI:
+  // <note place="end" n="3">Footnote body</note>
+  // TODO do not ignore n?
+  private def convertFootnote(element: Xml.Element, correlationIds: IdGenerator): Option[Xml.Nodes] =
+    val isFootnote: Boolean = element.getName == "note" && element.get("place").contains("end")
+    if !isFootnote then None else Some:
+      val correlationId = correlationIds.generate()
+      Chunk(
+        Footnote.link(correlationId),
+        Footnote.body(correlationId, element.getChildren)
+      )
