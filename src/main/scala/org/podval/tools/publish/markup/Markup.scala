04aa5338e7a62ffb2811b8fa4a00fcc28510c88d
Leonid Dubinsky <dub@podval.org>
2026-08-27 16:21:19 -0400

    - cleanup;

--- src/main/scala/org/podval/tools/publish/markup/Markup.scala
+++ src/main/scala/org/podval/tools/publish/markup/Markup.scala
@@ -1,10 +1,9 @@
 package org.podval.tools.publish.markup
 
-import org.podval.tools.publish.page.{FrontMatter, FullMarkupPage, Page}
+import org.podval.tools.publish.page.{FrontMatter, FullMarkupPage}
 import org.podval.tools.publish.site.{PageError, PageErrorReporter, Path, Site}
-import org.podval.tools.publish.util.{Date, Files}
+import org.podval.tools.publish.util.Files
 import org.podval.xml.{Html, Xml, XmlDialect, XmlEncode, XmlParser}
-import zio.blocks.html.*
 import java.io.File
 
 // TODO because of the cross-markup transclusion,
@@ -36,7 +35,7 @@ abstract class Markup(
   // - extract title
   def process(xml: Xml.Element, errorReporter: PageErrorReporter): (Xml.Element, Option[Xml.Element])
 
-  def pageHeader(page: FullMarkupPage): Html.Element = Markup.pageHeader(page)
+  def pageHeader(page: FullMarkupPage): Html.Element = PageHeader.pageHeader(page)
 
   final def readAndParse(
     site: Site,
@@ -120,55 +119,3 @@ object Markup:
     .toMap
 
   def forElement(element: String): Option[Markup] = forElement.get(element)
-
-  def pageHeader(page: FullMarkupPage): Html.Element =
-    header(className := "post-header",
-      postPath(page),
-      h1(className := "post-title p-name", itemProp := "name headline", page.title),
-      Option.when(!page.hasSyntheticContent)(articleMeta(page))
-    )
-
-  private def postPath(page: Page): Html.Element =
-    def parents(page: Page): Seq[Page] = page.parent match
-      case None => Seq.empty
-      case Some(parent) => parents(parent) :+ parent
-
-    val pathFull: Seq[Page] = parents(page)
-    val path: Seq[Page] = if pathFull.isEmpty then pathFull else pathFull.tail
-    span(className := "post-path", path.map(page => span("/", page.ref(withIcon = false))))
-
-  private def articleMeta(page: FullMarkupPage): Html.Element =
-    div(className := "post-meta",
-      join(
-        join(
-          join(
-            timeHtml(Option.when(page.dateModified.nonEmpty)("Published:"), page.date, "dt-published", "datePublished"),
-            "•",
-            timeHtml(Some("Updated:"), page.dateModified, "dt-modified", "dateModified")
-          ),
-          "•",
-          page.author.fold(Seq.empty): author =>
-            Seq(
-              span(className := "post-authors",
-                span(className := "post-author", itemProp := "author", itemScope := true, itemType := "http://schema.org/Person",
-                  span(className := "p-author h-card", itemProp := "name", author)
-                )
-              )
-            )
-        ),
-        "|",
-        page.tags.map(page.site.tags.tagRef)
-      )
-    )
-
-  private def join(left: Seq[Html.Element], text: String, right: Seq[Html.Element]): Seq[Html.Element] =
-    if left.nonEmpty && right.nonEmpty
-    then left ++ Seq(span(className := "bullet-divider", text)) ++ right
-    else left ++ right
-
-  private def timeHtml(label: Option[String], date: Option[Date], cls: String, itemprop: String): Seq[Html.Element] =
-    date.fold(Seq.empty): date =>
-      label.fold(Seq.empty)(label => Seq(span(className := "meta-label", label))) ++
-        Seq(time(className := cls, datetime := date.toString, itemProp := itemprop, date.toShortString))
-
-  
---
+++ src/main/scala/org/podval/tools/publish/markup/PageHeader.scala
@@ -0,0 +1,60 @@
+package org.podval.tools.publish.markup
+
+import org.podval.tools.publish.page.{FullMarkupPage, Page}
+import org.podval.tools.publish.util.Date
+import org.podval.xml.Html
+import zio.blocks.html.*
+
+object PageHeader:
+  def pageHeader(page: FullMarkupPage): Html.Element =
+    header(className := "post-header",
+      postPath(page),
+      h1(className := "post-title p-name", itemProp := "name headline", page.title),
+      Option.when(!page.hasSyntheticContent)(articleMeta(page))
+    )
+
+  private def postPath(page: Page): Html.Element =
+    def parents(page: Page): Seq[Page] = page.parent match
+      case None => Seq.empty
+      case Some(parent) => parents(parent) :+ parent
+
+    val pathFull: Seq[Page] = parents(page)
+    val path: Seq[Page] = if pathFull.isEmpty then pathFull else pathFull.tail
+    span(className := "post-path", path.map(page => span("/", page.ref(withIcon = false))))
+
+  private def articleMeta(page: FullMarkupPage): Html.Element =
+    div(className := "post-meta",
+      join(
+        join(
+          join(
+            timeHtml(Option.when(page.dateModified.nonEmpty)("Published:"), page.date, "dt-published", "datePublished"),
+            "•",
+            timeHtml(Some("Updated:"), page.dateModified, "dt-modified", "dateModified")
+          ),
+          "•",
+          page.author.fold(Seq.empty): author =>
+            Seq(
+              span(className := "post-authors",
+                span(className := "post-author", itemProp := "author", itemScope := true, itemType := "http://schema.org/Person",
+                  span(className := "p-author h-card", itemProp := "name", author)
+                )
+              )
+            )
+        ),
+        "|",
+        page.tags.map(page.site.tags.tagRef)
+      )
+    )
+
+  private def join(left: Seq[Html.Element], text: String, right: Seq[Html.Element]): Seq[Html.Element] =
+    if left.nonEmpty && right.nonEmpty
+    then left ++ Seq(span(className := "bullet-divider", text)) ++ right
+    else left ++ right
+
+  private def timeHtml(label: Option[String], date: Option[Date], cls: String, itemprop: String): Seq[Html.Element] =
+    date.fold(Seq.empty): date =>
+      label.fold(Seq.empty)(label => Seq(span(className := "meta-label", label))) ++
+        Seq(time(className := cls, datetime := date.toString, itemProp := itemprop, date.toShortString))
+
+  
+  
