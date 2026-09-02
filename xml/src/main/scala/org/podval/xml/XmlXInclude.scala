4ed53447dab17ede75c1a779823b39117246198e
Leonid Dubinsky <dub@podval.org>
2026-09-02 18:51:14 -0400

    - feat: switch to org.podval.xml - delete opentorah's xml support;

--- xml/src/main/scala/org/podval/xml/XmlXInclude.scala
+++ xml/src/main/scala/org/podval/xml/XmlXInclude.scala
@@ -9,6 +9,21 @@ import java.net.URL
   * Xerces post-pass was approximating; doing it here avoids
   * [[https://issues.apache.org/jira/browse/XERCESJ-1102 XERCESJ-1102]].
   */
+// This is mind-bogglingly weird, but:
+// - Xerces has a bug in the handling of XIncludes;
+// - starting at the third level of nested includes, the values of the xml:base attributes are wrong;
+// - the bug https://issues.apache.org/jira/browse/XERCESJ-1102 was reported in October 2005!!!
+// - a patch that allegedly fixes the issue is known for years
+// - a comment from the Xerces maintainer says:
+//   What Xerces needs most is new contributors / committers who can volunteer their time and help review these patches and get them committed.
+//   We also need a new release. It's been 5 years. Long overdue.
+//   If you or anyone else is interested in getting involved we'd be happy to have you join the project.
+// - latest release of Xerces was in 2023, with the bug still there
+// - many projects depend on Xerces, including Saxon, where the bug was also discussed: https://saxonica.plan.io/issues/4664
+// - allegedly, the bug is fixed in "SaxonC 11.1" - although how can this be with Saxon not shipping its own Xerces is not clear.
+//
+// So, I need to process XIncludes myself instead of relying on the industry-standard Xerces!
+// What a nightmare...
 object XmlXInclude:
   val NamespaceUri: String = "http://www.w3.org/2001/XInclude"
 
