923c60aac7a7a8ab3017ec1a7b39bab523a483fd
Leonid Dubinsky <dub@podval.org>
2026-09-08 11:12:18 -0400

    - cleanup: XML;

--- src/main/scala/org/podval/tools/publish/markup/CollectionPart.scala
+++ src/main/scala/org/podval/tools/publish/markup/CollectionPart.scala
@@ -1,6 +1,6 @@
 package org.podval.tools.publish.markup
 
-import org.podval.xml.{Xml, XmlCodec}
+import org.podval.xml.{Xml, XmlCodec, XmlTree, given}
 import zio.blocks.schema.{Modifier, Schema}
 
 /** A collection `<part from="000">` title row. `from` is the first original document's file name. */
@@ -8,11 +8,10 @@ final case class CollectionPart(
   @Modifier.config(XmlCodec.Attribute, "") n: Option[String] = None,
   @Modifier.config(XmlCodec.Attribute, "") from: String,
   // TODO why do I need Element modifier here?
-  @Modifier.config(XmlCodec.Element, "title") title: Option[Xml.Element] = None
+  @Modifier.config(XmlCodec.Element, "title") title: Option[XmlTree] = None
 ) derives CanEqual
 
 object CollectionPart:
-  import XmlCodec.xmlElementSchema
   given schema: Schema[CollectionPart] = Schema.derived
   val codec: XmlCodec[CollectionPart] = XmlCodec.derived
 
