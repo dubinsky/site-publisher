4cac1c9fd290e4a59e46985ae16d231f439299cb
Leonid Dubinsky <dub@podval.org>
2026-09-08 17:46:21 -0400

    - cleanup: XML;

--- src/main/scala/org/podval/tools/publish/markup/TeiXmlWriterConfig.scala
+++ src/main/scala/org/podval/tools/publish/markup/TeiXmlWriterConfig.scala
@@ -5,5 +5,6 @@ import org.podval.xml.XmlWriterConfig
 object TeiXmlWriterConfig extends XmlWriterConfig(
   unStack = Set("choice"),
   nest = Set("p", /*"abstract",*/ "head", "salute", "dateline"),
+  break = Set("lb"),
   cling = Set("note", "lb", "sic", "corr")
 )
