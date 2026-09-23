53d288e7e0ebfff2b98757055d56642f5ccb26cd
Leonid Dubinsky <dub@podval.org>
2026-09-23 16:44:49 -0400

    - cleanup;

--- publisher/src/main/scala/org/podval/tools/publish/markup/LinkKind.scala
+++ publisher/src/main/scala/org/podval/tools/publish/markup/LinkKind.scala
@@ -8,15 +8,6 @@ sealed abstract class LinkKind
 object LinkKind:
   final case class Entity(kind: EntityKind) extends LinkKind
 
-  // TODO use
-  case object Facsimile extends LinkKind
-
-  // TODO use
-  case object Tag extends LinkKind
-
-  // TODO use
-  case object Category extends LinkKind
-
   def of(element: Xml.Element): Option[LinkKind] = EntityKind
     .values
     .find(entityKind => element.hasClass(entityKind.nameElement))
